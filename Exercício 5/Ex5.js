6efc8f5082d3d17c18608618b0b2f41f1a02933d
Anajmp <anajmp11@gmail.com>
2025-05-05 15:57:28 -0300

    Modificação do Exercício 5

--- Exercício 5/Ex5.js
+++ Exercício 5/Ex5.js
@@ -2,26 +2,32 @@ const numero1 = parseInt(prompt("Escreva o primeiro número:"));
 const numero2 = parseInt(prompt("Escreva o segundo número:"));
 
 if (!isNaN(numero1) && !isNaN(numero2)) {
+
   let começo;
   let final;
 
   if (numero1 < numero2) {
+
     começo = numero1;
     final = numero2;
+
   } else {
+
     começo = numero2;
     final = numero1;
+
   }
 
-  let numP = ""; //Irá definir a váriavel numP como uma string
+  let numP = ""; //Irá definir a váriavel numP como uma string e armazenar os números pares entre o numero1 e numero2
 
   for (let i = começo; i <= final; i++) {
     if (i % 2 === 0) {
       numP += i + " ";
+
     }
   }
 
-  alert("Números pares entre " + começo + " e " + final + ": " + numP);
+  alert(`Números pares entre ${começo} e ${final} : ${numP}`);
 } else {
-  alert("Você digitou um número não válido.");
+  alert("Você escreveu um número não válido");
 }
